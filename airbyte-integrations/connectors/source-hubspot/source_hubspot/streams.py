c0e00ecd833fe681000d83c0ea0504a63c9c7327
Roman Steinberg <rys495@gmail.com>
2025-03-18 12:29:53 -0500

    add assoc + update schema

--- airbyte-integrations/connectors/source-hubspot/source_hubspot/streams.py
+++ airbyte-integrations/connectors/source-hubspot/source_hubspot/streams.py
@@ -2242,6 +2242,7 @@ class Goals(CRMObjectIncrementalStream):
 class LineItems(CRMObjectIncrementalStream):
     entity = "line_item"
     primary_key = "id"
+    associations = ["deals"]
     scopes = {"e-commerce"}
 
 
